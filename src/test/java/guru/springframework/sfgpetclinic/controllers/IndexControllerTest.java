76ab893601a12d4f82486598e87c0cd3e4b26c70
Pavel Glukhov <pvglukhov@gmail.com>
2023-05-30 19:26:57 +0300

    [master] added new features

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Executable;
+import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class IndexControllerTest {
     private static IndexController indexController;
@@ -28,5 +32,20 @@ class IndexControllerTest {
     @Test
     void oupsHandler() {
         assertThrows(ValueNotFoundException.class, () -> indexController.oupsHandler());
+    }
+
+    @Test
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+            System.out.println("I got here");
+        });
+    }
+    @Test
+    void testTimeoutGood() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(150);
+            fail("Can't be run");
+        });
     }
 }
