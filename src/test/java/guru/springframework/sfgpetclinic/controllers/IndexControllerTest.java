a9cfe57a87c3a95b31124fd577ad25a827a10918
Pavel Glukhov <pvglukhov@gmail.com>
2023-05-31 22:25:20 +0300

    [master] added new features

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -2,15 +2,20 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
     private static IndexController indexController;
@@ -27,6 +32,7 @@ class IndexControllerTest {
     @Test
     void index() {
         assertEquals("index", indexController.index());
+        assertThat(indexController.index()).isEqualTo("index");
     }
 
     @Test
@@ -34,6 +40,7 @@ class IndexControllerTest {
         assertThrows(ValueNotFoundException.class, () -> indexController.oupsHandler());
     }
 
+    @Disabled
     @Test
     void testTimeout() {
         assertTimeout(Duration.ofMillis(100), () -> {
@@ -41,11 +48,29 @@ class IndexControllerTest {
             System.out.println("I got here");
         });
     }
+
+    @Disabled
     @Test
     void testTimeoutGood() {
         assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
             Thread.sleep(150);
             fail("Can't be run");
         });
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testOnWindows() {
+    }
+
+    @EnabledOnOs(OS.LINUX)
+    @Test
+    void testOnLinux() {
+        fail("Test must run");
     }
 }
