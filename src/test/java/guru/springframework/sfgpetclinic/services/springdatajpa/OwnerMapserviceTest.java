0dcbb568849809c32cb472fe71ccd18a3f7fe2d2
Pavel Glukhov <pgluhov@verci.ru>
2023-06-01 21:42:43 +0300

    [master] added new features

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerMapserviceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerMapserviceTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.map.OwnerMapService;
@@ -23,6 +25,7 @@ public class OwnerMapserviceTest {
         petTypeService = new PetTypeMapService();
         petService = new PetMapService();
         ownerMapService = new OwnerMapService(petTypeService, petService);
+        System.out.println("First before Each");
     }
 
     @DisplayName("Verify Zero Owners")
@@ -35,7 +38,37 @@ public class OwnerMapserviceTest {
     @DisplayName("Pet Type - ")
     @Nested
     class TestCreatePetTypes {
+        @BeforeEach
+        void setUp() {
+            PetType petType = new PetType(1L, "Dog");
+            PetType petType2 = new PetType(2L, "Cat");
+            petTypeService.save(petType);
+            petTypeService.save(petType2);
+            System.out.println("Nested before Each");
+        }
 
+        @Test
+        void testPetCount() {
+            int petTypeCount = petTypeService.findAll().size();
+            assertThat(petTypeCount).isNotZero().isEqualTo(2);
+        }
+
+        @DisplayName("Save owner Tests - ")
+        @Nested
+        class SaveOwnersTests {
+            @BeforeEach
+            void setUp() {
+                ownerMapService.save(new Owner(1L, "Before", "Each"));
+                System.out.println("Saved Owner Before Each");
+            }
+
+            @Test
+            void saveOwner() {
+                Owner owner = new Owner(2L, "Joe", "Buck");
+                Owner savedOwner = ownerMapService.save(owner);
+                assertThat(savedOwner).isNotNull();
+            }
+        }
     }
 
 
